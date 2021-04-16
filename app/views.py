167c006543ad3a5cfbf353909f59e5159009a65b
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-16 09:25:50 +0300

    the API can now call on a particular category of movies

--- app/views.py
+++ app/views.py
@@ -1,9 +1,14 @@
 from flask import render_template
 from app import app
+from .request import get_sources
 
 @app.route('/')
 def index():
     """
     View root page function that returns the index page and its data
     """
-    return render_template('index.html')
+    general_news = get_sources('general')
+    business_news = get_sources('business')
+    sports_news = get_sources('sports')
+    title = 'Home - Welcome to the NewsBlitz'
+    return render_template('index.html', title = title, general = general_news)
