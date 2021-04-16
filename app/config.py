feb110605317104fb33a94142f8dbcd61e4ed866
Derrick-Nyongesa <nyongesaderrick@gmail.com>
2021-04-16 11:21:51 +0300

    generated articles base url

--- app/config.py
+++ app/config.py
@@ -4,6 +4,8 @@ class Config:
     """
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?category={}&apiKey={}'
 
+    ARTICLES_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+
 class ProdConfig(Config):
     """
     Production  configuration child class
